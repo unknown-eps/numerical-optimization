34420c2d5372373b446e8945d4009fe243f19e12
harshit rawat <harshit2k19@gmail.com>
2025-04-26 23:01:08 +0530

    began simplex

---
+++ final_exam/q1_simplex.py
@@ -0,0 +1,7 @@
+import numpy as np
+## Constants
+INTEREST_RATE = 0.005  # Interest rate for investment
+K_REPAY_FACTOR = 1.025  # Repayment factor for 4-month loan
+J_REPAY_FACTOR = (1+0.018)**2  # Repayment factor for 6-month loan
+L_REPAY_FACTOR = (1+0.01)**8  # Repayment factor for 2-year loan
+
